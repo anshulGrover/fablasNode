c2d15990008859aa44a8ff12133635404d17be08
anshulGrover <anshul1601.grover@gmail.com>
2019-04-09 15:35:14 +0530

    api modified msgs

--- mails/mail.service.js
+++ mails/mail.service.js
@@ -2,6 +2,10 @@ var nodemailer=require('nodemailer');
 
 const sendVendor=(data) => {
     return new Promise((resolve, reject) => {
+        if(data.fullName===undefined||data.businessName===undefined||data.businessAddress===undefined||data.city===undefined||data.country===undefined||data.permanentBusinessType===undefined||data.website===undefined){            
+            resolve("Unable to process your request please try again with all fields!");
+        }
+        else{
         var transporter =  nodemailer.createTransport({
             host:"166.62.10.188",
             port:587,
@@ -14,7 +18,7 @@ const sendVendor=(data) => {
               pass: "Summer@69"
             }
           });
-          var mailBody=`Full Name : ${data.fullName}\nBusiness Name : ${data.businessName}\nBusiness Address : ${data.businessAddress}\nCity : ${data.city}\nCountry : ${data.city}\nPrimary Business Type : ${data.permanentBusinessType}\nWebsite : ${data.website}`;
+          var mailBody=`Full Name : ${data.fullName}\nBusiness Name : ${data.businessName}\nBusiness Address : ${data.businessAddress}\nCity : ${data.city}\nCountry : ${data.country}\nPrimary Business Type : ${data.permanentBusinessType}\nWebsite : ${data.website}`;
           var mailOptions={
               from:"info@fablas.co.in",
               to:"stackdesignerkush@gmail.com",
@@ -23,12 +27,13 @@ const sendVendor=(data) => {
           }
           transporter.sendMail(mailOptions,function(error,info){
               if(!error){
-                resolve('Email sent: ' + info.response);
+                resolve("Thanks for your input. Will get back to you shortly");
               }
               else{
                   reject(error);
               }
-          })     
+          })
+        }     
     })
 }
 
