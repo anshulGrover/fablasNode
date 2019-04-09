0c27cf6c9008fbcc99fec7244f437fc444fc748b
anshulGrover <anshul1601.grover@gmail.com>
2019-04-09 17:17:50 +0530

    2 apis added

--- mails/mail.controller.js
+++ mails/mail.controller.js
@@ -29,4 +29,58 @@ router.post("/vendor",(req,res,next)=>{
     });
 })
 
+router.post("/distributor",(req,res,next)=>{
+    const{
+        fullName,
+        companyName,
+        companyAddres,
+        mobile,
+        email,
+        city,
+        country,
+        investmentRange,
+        website,
+        aboutYou
+    }=req.body;
+    var data={
+        fullName,
+        companyName,
+        companyAddres,
+        mobile,
+        email,
+        city,
+        country,
+        investmentRange,
+        website,
+        aboutYou
+    }
+    mailServices.sendDistributor(data).then((result) => {
+        res.json(result);
+    }).catch((err) => {
+        console.log(err);
+		res.sendStatus(400);
+    });
+})
+router.post("/jobSeeker",(req,res,next)=>{
+    const{
+        fullName,
+        mobile,
+        email,
+        applyingFor,
+        message
+    }=req.body;
+    var data={
+        fullName,
+        mobile,
+        email,
+        applyingFor,
+        message
+    }
+    mailServices.sendJobSeeker(data).then((result) => {
+        res.json(result);
+    }).catch((err) => {
+        console.log(err);
+		res.sendStatus(400);
+    });
+})
 module.exports= router;
--- mails/mail.service.js
+++ mails/mail.service.js
@@ -37,6 +37,79 @@ const sendVendor=(data) => {
     })
 }
 
+const sendDistributor=(data) => {
+    return new Promise((resolve, reject) => {
+        if(data.fullName===undefined||data.companyName===undefined||data.companyAddres===undefined||data.mobile===undefined||data.email===undefined||data.city===undefined||data.country===undefined||data.investmentRange===undefined||data.website===undefined||data.aboutYou===undefined){            
+            reject("Unable to process your request. Please try again with all fields!");
+        }
+        else{
+        var transporter =  nodemailer.createTransport({
+            host:"166.62.10.188",
+            port:587,
+            secure:false,
+            tls: {
+                rejectUnauthorized:false
+            },
+            auth: { 
+              user: "info@fablas.co.in",
+              pass: "Summer@69"
+            }
+          });
+          var mailBody=`Full Name : ${data.fullName}\nCompany Name : ${data.companyName}\nCompany Address : ${data.companyAddres}\nMobile : ${data.mobile}\nEmail : ${data.email}\nCity : ${data.city}\nCountry : ${data.country}\nInvestment Range : ${data.investmentRange}\nWebsite : ${data.website}\nAbout you : ${data.aboutYou}`;
+          var mailOptions={
+              from:"info@fablas.co.in",
+              to:"stackdesignerkush@gmail.com",
+              subject:"From Distributor api",
+              text:mailBody
+          }
+          transporter.sendMail(mailOptions,function(error,info){
+              if(!error){
+                resolve("Thanks for your input. Will get back to you shortly");
+              }
+              else{
+                  reject(error);
+              }
+          })
+        }     
+    })
+}
+
+const sendJobSeeker=(data) => {
+    return new Promise((resolve, reject) => {
+        if(data.fullName===undefined||data.mobile===undefined||data.email===undefined||data.applyingFor===undefined||data.message===undefined){            
+            reject("Unable to process your request. Please try again with all fields!");
+        }
+        else{
+        var transporter =  nodemailer.createTransport({
+            host:"166.62.10.188",
+            port:587,
+            secure:false,
+            tls: {
+                rejectUnauthorized:false
+            },
+            auth: { 
+              user: "info@fablas.co.in",
+              pass: "Summer@69"
+            }
+          });
+          var mailBody=`Full Name : ${data.fullName}\nMobile : ${data.mobile}\nEmail : ${data.email}\nApplying For : ${data.applyingFor}\nMessage : ${data.message}`;
+          var mailOptions={
+              from:"info@fablas.co.in",
+              to:"stackdesignerkush@gmail.com",
+              subject:"From Distributor api",
+              text:mailBody
+          }
+          transporter.sendMail(mailOptions,function(error,info){
+              if(!error){
+                resolve("Thanks for your input. Will get back to you shortly");
+              }
+              else{
+                  reject(error);
+              }
+          })
+        }     
+    })
+}
 module.exports={
-    sendVendor
+    sendVendor,sendDistributor,sendJobSeeker
 };
