e3d878ed7deb4dc452128acf0d1a4fc416bfd307
anshulGrover <anshul1601.grover@gmail.com>
2019-07-13 15:51:25 +0530

    test change

--- product/product.services.js
+++ product/product.services.js
@@ -137,7 +137,7 @@ const addProducts=(subCategory,data)=>{
 				console.log(requiredSub);
 				requiredSub[0].productsList.push(data);
 				product.save().then((product) => {
-					resolve(product);
+					resolve("product added");
 				}).catch((err) => {
 					reject(err);
 				})
